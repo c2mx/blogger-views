77c9d21fdd69b4c5ecbcd0050f7a7f4cd034d77d
C2MX <xuming880@gmail.com>
2025-06-27 18:02:33 +0800

    init

---
+++ fetch_stats.py
@@ -0,0 +1,46 @@
+import os
+import requests
+import json
+from datetime import datetime
+
+# 环境变量
+CLIENT_ID = os.environ['CLIENT_ID']
+CLIENT_SECRET = os.environ['CLIENT_SECRET']
+REFRESH_TOKEN = os.environ['REFRESH_TOKEN']
+BLOG_ID = os.environ['BLOG_ID']
+
+# 获取访问令牌
+res = requests.post("https://oauth2.googleapis.com/token", data={
+    'client_id': CLIENT_ID,
+    'client_secret': CLIENT_SECRET,
+    'refresh_token': REFRESH_TOKEN,
+    'grant_type': 'refresh_token'
+})
+access_token = res.json().get("access_token")
+if not access_token:
+    raise Exception("Failed to refresh access token")
+
+# 获取访问数据
+resp = requests.get(
+    f'https://www.googleapis.com/blogger/v3/blogs/{BLOG_ID}/pageViews?range=all',
+    headers={'Authorization': f'Bearer {access_token}'}
+)
+data = resp.json()
+view_count = data['counts'][0]['count']
+
+# 加载旧数据并追加
+json_path = 'data/stats.json'
+try:
+    with open(json_path, 'r') as f:
+        history = json.load(f)
+except FileNotFoundError:
+    history = []
+
+history.append({
+    'timestamp': datetime.utcnow().isoformat() + 'Z',
+    'pageViews': view_count
+})
+
+# 保存新数据
+with open(json_path, 'w') as f:
+    json.dump(history, f, indent=2)
